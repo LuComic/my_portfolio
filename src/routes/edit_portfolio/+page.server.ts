b5ff49e0e6493565f703693c816c4f67e05b0d50
Lukas Jääger <81866926+LuComic@users.noreply.github.com>
2025-07-31 23:31:28 +0300

    if you dont have access to edit_port, it redirects to admin_pin

--- src/routes/edit_portfolio/+page.server.ts
+++ src/routes/edit_portfolio/+page.server.ts
@@ -1,12 +1,11 @@
 import { redirect } from '@sveltejs/kit';
 import type { PageServerLoad, Actions } from './$types';
-import { error } from '@sveltejs/kit';
 
 export const load: PageServerLoad = ({ cookies }) => {
 	const allowed = cookies.get('allowed');
 
 	if (!allowed) {
-		error(403, 'Forbidden');
+		redirect(303, '../admin_pin/');
 	}
 };
 
