ef6f9906fdfbd4768bd4d5af5b5f96a3143b7a83
Lukas Jääger <81866926+LuComic@users.noreply.github.com>
2025-09-20 23:28:49 +0300

    removed since it was slow on first click

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,25 +1,7 @@
 <script lang="ts">
 	import '../app.css';
-	import { onMount } from 'svelte';
-	import { projects } from '$lib/info';
-	import { preloadImages, toAssetPath } from '$lib/utils/imagePreloader';
-
-	const qportImages = ['pictures/qportBanner copy.png'];
-	const imagesToPreload = Array.from(
-		new Set([...projects.flatMap((p) => p.images ?? []), ...qportImages])
-	);
-
-	onMount(() => {
-		preloadImages(imagesToPreload);
-	});
 
 	let { children } = $props();
 </script>
-
-<svelte:head>
-	{#each imagesToPreload as img}
-		<link rel="preload" as="image" href={toAssetPath(img)} />
-	{/each}
-</svelte:head>
 
 {@render children()}
