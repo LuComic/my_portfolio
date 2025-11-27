64f77e49111ba3aa98ac73883bf35705b78c3bf2
Lukas Jääger <81866926+LuComic@users.noreply.github.com>
2025-11-27 16:13:31 +0200

    font sizing change for blog

--- src/routes/(with_header)/blog/[id]/+page.svelte
+++ src/routes/(with_header)/blog/[id]/+page.svelte
@@ -44,27 +44,29 @@
 		class="flex h-full w-full flex-col items-start justify-start px-4 py-8 md:w-3/4 md:py-0 lg:w-1/2 lg:py-0 xl:w-1/2 xl:py-0"
 	>
 		<h1
-			class="mt-4 flex w-full items-center justify-between gap-4 text-lg font-medium text-[#e7e5e4] visited:text-[#e7e5e4] md:text-xl"
+			class="mt-4 flex w-full items-center justify-between gap-4 text-lg font-medium text-[#e7e5e4] visited:text-[#e7e5e4] md:text-xl xl:text-2xl"
 		>
 			<a href="/blog" class="flex items-center justify-start gap-1 visited:text-[#e7e5e4] md:gap-2">
 				<ChevronLeft color="currentColor" />
 				{foundBlog?.title}
 				{#if foundBlog?.type === 'qport'}
-					<span class="ml-2 rounded-lg border border-green-400 px-2 py-1 text-sm md:ml-0">
+					<span
+						class="ml-2 rounded-lg border border-green-400 px-2 py-1 text-sm md:ml-0 md:text-base"
+					>
 						{foundBlog?.version}
 					</span>
 				{/if}
 			</a>
 			{#if foundBlog?.type === 'qport'}
 				<div
-					class="flex w-auto flex-col items-start justify-center gap-0 text-base md:flex-row md:items-center md:gap-2 md:text-xl"
+					class="flex w-auto flex-col items-start justify-center gap-0 text-base md:flex-row md:items-center md:gap-2 md:text-xl xl:text-2xl"
 				>
 					<p class="text-zinc-400">{foundBlog?.date}</p>
 					<span class="vr hidden h-4 border-slate-500 md:inline"></span>
 					<p class="text-zinc-400">{foundBlog?.updateType}</p>
 				</div>
 			{:else}
-				<p class="text-zinc-400">{foundBlog?.date}</p>
+				<p class="text-base text-zinc-400 md:text-xl xl:text-2xl">{foundBlog?.date}</p>
 			{/if}
 		</h1>
 		<hr class="hr mt-4 mb-3 border-zinc-600" />
@@ -74,12 +76,12 @@
 		>
 			{#if foundBlog?.content}
 				{#each foundBlog?.content?.split('\n').filter((line) => line.trim()) || [] as paragraph}
-					<p class="text-base md:text-lg">{paragraph.trim()}</p>
+					<p class="text-base md:text-lg xl:text-xl">{paragraph.trim()}</p>
 				{/each}
 			{:else if foundBlog?.contentAd1 || foundBlog?.contentAd2 || foundBlog?.contentAd3 || foundBlog?.contentAd4}
 				{#if foundBlog?.contentAd1}
 					{#each foundBlog.contentAd1.split('\n').filter((line) => line.trim()) || [] as paragraph}
-						<p class="text-base md:text-lg">{paragraph.trim()}</p>
+						<p class="text-base md:text-lg xl:text-xl">{paragraph.trim()}</p>
 					{/each}
 					{#if foundBlog?.codeAd1}
 						<pre class="rounded-xl bg-zinc-950/60 p-4 text-sm text-gray-100 md:text-base"><code
@@ -89,7 +91,7 @@
 				{/if}
 				{#if foundBlog?.contentAd2}
 					{#each foundBlog.contentAd2.split('\n').filter((line) => line.trim()) || [] as paragraph}
-						<p class="text-base md:text-lg">{paragraph.trim()}</p>
+						<p class="text-base md:text-lg xl:text-xl">{paragraph.trim()}</p>
 					{/each}
 					{#if foundBlog?.codeAd2}
 						<pre class="rounded-xl bg-zinc-950/60 p-4 text-sm text-gray-100 md:text-base"><code
@@ -99,7 +101,7 @@
 				{/if}
 				{#if foundBlog?.contentAd3}
 					{#each foundBlog.contentAd3.split('\n').filter((line) => line.trim()) || [] as paragraph}
-						<p class="text-base md:text-lg">{paragraph.trim()}</p>
+						<p class="text-base md:text-lg xl:text-xl">{paragraph.trim()}</p>
 					{/each}
 					{#if foundBlog?.codeAd3}
 						<pre
@@ -110,7 +112,7 @@
 				{/if}
 				{#if foundBlog?.contentAd4}
 					{#each foundBlog.contentAd4.split('\n').filter((line) => line.trim()) || [] as paragraph}
-						<p class="text-base md:text-lg">{paragraph.trim()}</p>
+						<p class="text-base md:text-lg xl:text-xl">{paragraph.trim()}</p>
 					{/each}
 					{#if foundBlog?.codeAd4}
 						<pre
